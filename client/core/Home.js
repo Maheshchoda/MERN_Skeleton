194066e7cd0b3344538cfb983a391da1fe6385b9
Maheshchoda <choda.maheshbabu@gmail.com>
2018-12-25 22:56:21 +0530

    Added image to home page

--- client/core/Home.js
+++ client/core/Home.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import homeImage from "../assets/images/Home.jpg";
 
-const Home = () => {
-  return <h1> This is Home Component</h1>;
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    margin: "auto",
+    marginTop: theme.spacing.unit * 5
+  },
+  title: {
+    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme
+      .spacing.unit * 2}px`,
+    color: theme.palette.text.secondary
+  },
+  media: {
+    minHeight: 330
+  }
+});
+
+class Home extends Component {
+  render() {
+    const { classes } = this.props;
+    const { card, title, media } = classes;
+    return (
+      <Card className={card}>
+        <CardMedia
+          className={media}
+          image={homeImage}
+          title="welcome to home"
+        />
+        <CardContent>
+          <Typography type="body1" component="p">
+            This is Home page of our App
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
 };
-
-export default Home;
+export default withStyles(styles)(Home);
